55864a1284e052118beb9e015acdabc1f5ef66e2
RadimRiha <radim.rihacek@seznam.cz>
2022-02-24 15:29:02 +0100

    settings

--- SW/camera_app/camera_app/MyForm.cpp
+++ SW/camera_app/camera_app/MyForm.cpp
@@ -39,13 +39,56 @@ void main(array<String^>^ args)
         camera.Open();
 
         uint16_t width = 2048;
+        bool success = true;
+        
+        success &= camera.Width.TrySetValue(width, IntegerValueCorrection_Nearest);
+        success &= camera.Height.TrySetValue(width, IntegerValueCorrection_Nearest);
+        success &= camera.OffsetX.TrySetValue((camera.Width.GetMax()-width)/2);
+        success &= camera.TriggerSelector.TrySetValue(TriggerSelector_LineStart);
+        success &= camera.TriggerMode.TrySetValue(TriggerMode_On);
+        success &= camera.ExposureMode.TrySetValue(ExposureMode_TriggerWidth);
+        success &= camera.AcquisitionStatusSelector.TrySetValue(AcquisitionStatusSelector_LineTriggerWait);
+        success &= camera.LineSelector.TrySetValue(LineSelector_Out1);
+        success &= camera.LineSource.TrySetValue(LineSource_LineTriggerWait);
+        success &= camera.GevSCPSPacketSize.TrySetValue(camera.Width.GetMax());
+        
+        try {
+            // The parameter MaxNumBuffer can be used to control the count of buffers
+            // allocated for grabbing. The default value of this parameter is 10.
+            camera.MaxNumBuffer = 5;
+            // Start the grabbing of c_countOfImagesToGrab images.
+            // The camera device is parameterized with a default configuration which
+            // sets up free-running continuous acquisition.
+            camera.StartGrabbing(10);
+            // This smart pointer will receive the grab result data.
+            CGrabResultPtr ptrGrabResult;
+            // Camera.StopGrabbing() is called automatically by the RetrieveResult() method
+            // when c_countOfImagesToGrab images have been retrieved.
+            while (camera.IsGrabbing())
+            {
+                // Wait for an image and then retrieve it. A timeout of 5000 ms is used.
+                camera.RetrieveResult(5000, ptrGrabResult, TimeoutHandling_ThrowException);
 
-        camera.Width.TrySetValue(width, IntegerValueCorrection_Nearest);
-        camera.Height.TrySetValue(width, IntegerValueCorrection_Nearest);
-        camera.OffsetX.TrySetValue((camera.Width.GetMax()-width)/2);
-
-        camera.TriggerSelector.TrySetValue(TriggerSelector_LineStart);
-        camera.TriggerMode.TrySetValue(TriggerMode_On);
+                // Image grabbed successfully?
+                if (ptrGrabResult->GrabSucceeded())
+                {
+                    // Access the image data.
+                    const uint8_t* pImageBuffer = (uint8_t*)ptrGrabResult->GetBuffer();
+                    form.outLabel->Text = gcnew String(form.outLabel->Text + "Gray value of first pixel: " + (uint32_t)pImageBuffer[0] + "\n");
+                }
+                else
+                {
+                    std::cout << "Error: " << std::hex << ptrGrabResult->GetErrorCode() << std::dec << " " << ptrGrabResult->GetErrorDescription() << std::endl;
+                    form.outLabel->Text = gcnew String(form.outLabel->Text + ptrGrabResult->GetErrorCode() + " error\n");
+                }
+            }
+        }
+        catch (const GenericException& e)
+        {
+            // Error handling.
+            std::cerr << "An exception occurred." << std::endl
+                << e.GetDescription() << std::endl;
+        }
 
         // Close the camera.
         camera.Close();
@@ -53,7 +96,6 @@ void main(array<String^>^ args)
     catch (const GenericException& e)
     {
         /*
-        // Error handling.
         cerr << "An exception occurred." << endl
             << e.GetDescription() << endl;
         exitCode = 1;*/
--- SW/camera_app/camera_app/MyForm.h
+++ SW/camera_app/camera_app/MyForm.h
@@ -35,6 +35,15 @@ namespace cameraapp {
 			}
 		}
 	public: System::Windows::Forms::ComboBox^ deviceSelect;
+	public: System::Windows::Forms::Label^ outLabel;
+
+	public:
+
+	public:
+
+	public:
+
+	public:
 	protected:
 
 	protected:
@@ -67,6 +76,7 @@ namespace cameraapp {
 		void InitializeComponent(void)
 		{
 			this->deviceSelect = (gcnew System::Windows::Forms::ComboBox());
+			this->outLabel = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// deviceSelect
@@ -78,15 +88,27 @@ namespace cameraapp {
 			this->deviceSelect->TabIndex = 0;
 			this->deviceSelect->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::deviceSelect_SelectedIndexChanged);
 			// 
+			// outLabel
+			// 
+			this->outLabel->AutoSize = true;
+			this->outLabel->Location = System::Drawing::Point(196, 227);
+			this->outLabel->Name = L"outLabel";
+			this->outLabel->Size = System::Drawing::Size(48, 13);
+			this->outLabel->TabIndex = 1;
+			this->outLabel->Text = L"outLabel";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(684, 485);
+			this->Controls->Add(this->outLabel);
 			this->Controls->Add(this->deviceSelect);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -94,5 +116,7 @@ namespace cameraapp {
 	private: System::Void deviceSelect_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
 
 	}
-	};
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
