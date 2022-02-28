8fa537a1ba3d48d5ebed3758dc759f96096428f4
RadimRiha <radim.rihacek@seznam.cz>
2022-02-28 21:39:03 +0100

    switched to C#

---
+++ SW/camera_app/camera_app/CameraConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basler.Pylon;
+
+namespace camera_app
+{
+	static class CameraConfig
+    {
+		static readonly string[] supportedCameras = {
+		"Basler raL6144-16gm"
+		};
+
+		static string cameraSerialNumber;
+
+		public static bool setCameraModel(string camera)
+		{
+			if (!Array.Exists(supportedCameras, element => camera.StartsWith(element))) return false;
+			cameraSerialNumber = camera.Substring(camera.IndexOf('(') + 1);
+			cameraSerialNumber = cameraSerialNumber.Substring(0,cameraSerialNumber.Length-1);
+			updateParams();
+			return true;
+		}
+
+		private static bool updateParams()
+		{
+			using (Camera camera = new Camera(cameraSerialNumber))
+			{
+				bool success = true;
+				camera.Open();
+				success &= camera.Parameters[PLCamera.Width].TrySetValue(1000, IntegerValueCorrection.Nearest);
+				success &= camera.Parameters[PLCamera.Height].TrySetValue(1000, IntegerValueCorrection.Nearest);
+				success &= camera.Parameters[PLCamera.OffsetX].TrySetValue(1000, IntegerValueCorrection.Nearest);
+				success &= camera.Parameters[PLCamera.TriggerSelector].TrySetValue(PLCamera.TriggerSelector.LineStart);
+				success &= camera.Parameters[PLCamera.TriggerMode].TrySetValue(PLCamera.TriggerMode.On);
+				success &= camera.Parameters[PLCamera.ExposureMode].TrySetValue(PLCamera.ExposureMode.TriggerWidth);
+				success &= camera.Parameters[PLCamera.LineSelector].TrySetValue(PLCamera.LineSelector.Out1);
+				success &= camera.Parameters[PLCamera.LineSource].TrySetValue(PLCamera.LineSource.LineTriggerWait);
+				success &= camera.Parameters[PLCamera.GevSCPSPacketSize].TrySetValue(camera.Parameters[PLCamera.Width].GetMaximum());
+				camera.Close();
+				return success;
+			}
+		}
+	}
+}
---
+++ SW/camera_app/camera_app/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Basler.Pylon;
+
+namespace camera_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DeviceSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(DeviceSelect.SelectedItem != null) CameraConfig.setCameraModel(DeviceSelect.SelectedItem.ToString());
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DeviceSelect.Items.Clear();
+            List<ICameraInfo> cameras = CameraFinder.Enumerate();
+            foreach (ICameraInfo camera in cameras)
+            {
+                DeviceSelect.Items.Add(camera.GetValueOrDefault("FriendlyName","property not found"));
+            }
+        }
+
+        private void StartAcquisition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StopAcquisition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
